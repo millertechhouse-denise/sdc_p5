d2a16f5e77cf66d3dc32c4861c1c02f4c3f7ec24
denise <denisej199@gmail.com>
2017-03-06 23:36:04 -0800

    updated writeup only

--- FindCars.py
+++ FindCars.py
@@ -269,7 +269,7 @@ if __name__ == '__main__':
 
     video = True
     #apply pipeline to video
-    output = 'output_images/test_video.mp4'
+    output = 'output_images/processed_video.mp4'
     clip1 = VideoFileClip("project_video.mp4")
     clip = clip1.fl_image(process_image)
     clip.write_videofile(output, audio=False)
